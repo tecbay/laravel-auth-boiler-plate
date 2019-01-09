de980a82bbe75c5baaec8cf9e557e4b2a12887f1
Your Name <miraj.net.xp@gmail.com>
2019-01-09 22:28:35 +0600

    Complete Auth boiler plate

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Foundation\Inspiring;
+use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,23 @@ use Illuminate\Foundation\Inspiring;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+Artisan::command( 'inspire', function () {
+	$this->comment( Inspiring::quote() );
+} )->describe( 'Display an inspiring quote' );
+
+
+Artisan::command( 'create:admin', function () {
+
+	$user = User::create( [
+		'name'     => 'Admin',
+		'email'    => str_random( 5 ) . '@gmail.com',
+		'password' => Hash::make( 'rroott' ),
+	] );
+
+	if ( ! ( Role::where( 'name', 'admin' )->first() ) ) {
+		$role = Role::create( [ 'name' => 'admin' ] );
+	}
+
+	$user->assignRole( 'admin' );
+
+} )->describe( 'Create a new admin user with admin role' );
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,59 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+ Route::group(['middleware' => ['role:super-admin|writer']], function () {
+
 });
 
+Route::group(['middleware' => ['permission:publish articles|edit articles']], function () {
+
+});
+
+*/
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+//<editor-fold desc="Guest Routes">
+Route::group( [ 'middleware' => [ 'guest' ] ], function () {
+	Route::get( '/', function () {
+		return view( 'welcome' );
+	} );
+} );
+//</editor-fold>
+
+// All Authenticate user routes
+Route::group( [ 'middleware' => [ 'auth' ] ], function () {
+	//<editor-fold desc="Redirecting user after login base on role">
+	Route::get( '/home', function () {
+		if ( auth()->user()->hasRole( 'admin' ) ) {
+			$redirect = redirect( route( 'admin.home' ) );
+		} else {
+			$redirect = redirect( route( 'user.home' ) );
+		}
+
+		return $redirect;
+	} )->name( 'home' );
+	//</editor-fold>
+
+	//<editor-fold desc="Admin Route">
+	Route::group( [ 'middleware' => [ 'role:admin' ], 'as' => 'admin.' ], function () {
+		// All Admin Routes +++++
+		Route::get( '/admin', function () {
+			return view( 'home' );
+		} )->name( 'home' );
+
+	} );
+	//</editor-fold>
+
+	//<editor-fold desc="User Route">
+	Route::group( [ 'middleware' => [ 'role:user' ] ], function () {
+		//All User Routes +++++
+		Route::get( '/user', function () {
+			return view( 'home' );
+		} )->name( 'user.home' );
+
+	} );
+	//</editor-fold>
+} );
+// All Authenticate user routes end
